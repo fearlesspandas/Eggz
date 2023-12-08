6ac3c27424e941648e6d19e1b0d3564ebd06007d
fearlesspandas <landonrenzullo@gmail.com>
2023-12-08 16:20:44 -0500

    fleshing out more of the Terrain hash tree implementation

--- src/com/main/scala/entity/Terrain.scala
+++ src/com/main/scala/entity/Terrain.scala
@@ -4,7 +4,12 @@ import entity.Terrain.TerrainId
 import entity.TerrainBlock.generate_lattice
 import zio.IO
 import zio.Ref
+import zio.Scope
 import zio.ZIO
+import zio.ZIOAppArgs
+import zio.ZIOAppDefault
+
+import scala.math.abs
 
 trait Terrain {
   def add_terrain(
@@ -23,34 +28,46 @@ object Terrain {
 }
 trait TerrainError
 
-//Vector(1 , -1 , 1)
-//modify to take Ref[TerrainBlock] in map to avoid writing update logic
 case class TerrainBlock(
-  center: Vector[Double],
-  terrainRef: Ref[Map[Vector[Double], TerrainBlock] | TerrainId],
-  size: Double
+  centerRef: Ref[Vector[Double]],
+  terrainRef: Ref[Map[Vector[Double], Ref[Terrain]] | TerrainId],
+  size: Ref[Int],
+  radius: Double
 ) extends Terrain {
 
   def base(
     id: TerrainId,
-    location: Vector[Double]
+    location: Vector[Double],
+    size: Int,
+    radius: Double
   ): IO[TerrainError, TerrainBlock] = for {
-    ref <- Ref.make[Map[Vector[Double], TerrainBlock] | TerrainId](id)
-  } yield TerrainBlock(location, ref, 0)
+    ref <- Ref.make[Map[Vector[Double], Ref[Terrain]] | TerrainId](
+      if radius > 1 then Map() else id
+    )
+    loc <- Ref.make(location)
+    sz <- Ref.make(0)
+  } yield TerrainBlock(loc, ref, sz, radius)
 
   def generate_quadrants(): IO[TerrainError, Seq[Vector[Double]]] = for {
+    center <- centerRef.get
     res <- ZIO.succeed(
       generate_lattice(Vector((0 to center.length).map(_ => 0.0): _*))
     )
   } yield res
 
+  // manhattan distance used to test for inclusion within block
   def is_within_block(location: Vector[Double]): IO[TerrainError, Boolean] =
-    ??? // compute manhattan distance from center for max bounds
+    for {
+      center <- centerRef.get
+      diffs = center.zip(location).map((a, b) => abs(a - b))
+      res = diffs.forall(diff => diff < radius)
+    } yield res
 
   def get_quadrant(
     location: Vector[Double]
   ): IO[TerrainError, Option[Vector[Double]]] =
     for {
+      center <- centerRef.get
       in_block <- is_within_block(location)
       res =
         if in_block then {
@@ -63,14 +80,33 @@ case class TerrainBlock(
     location: Vector[Double]
   ): IO[TerrainError, Unit] =
     (for {
-      quad <- get_quadrant(location).flatMap(ZIO.fromOption(_))
-      _ <- terrainRef.get.flatMap(x =>
-        x match {
-          case m: Map[Vector[Double], TerrainBlock] =>
-            ZIO.fromOption(m.get(quad)).flatMap(_.add_terrain(id, location))
-          case t: TerrainId => base(id, location)
-        }
-      )
+      quadr <- get_quadrant(location).flatMap(ZIO.fromOption(_))
+      _ <- terrainRef.get.flatMap {
+        case m: Map[Vector[Double], Ref[Terrain]] =>
+          for {
+            sz <- size.get
+            bkp_ <- base(id, location, 0, radius / 2)
+            bkp <- Ref.make[Terrain](bkp_)
+            // if quadrant is a terrain block -> defer add_terrain to the sub-block
+            // otherwise if no block exists, we add a default block.
+            // The default will either represent another sub block (if radius > 1)
+            // or a single point containing an entity id (if radius <= 1)
+            block <- ZIO
+              .fromOption(m.get(quadr))
+              .flatMap(_.get.map { case t: TerrainBlock => t })
+              .flatMapError(_ =>
+                terrainRef.update { case m: Map[Vector[Double], Ref[Terrain]] =>
+                  m.updated(quadr, bkp)
+                }
+              )
+              .fold(_ => bkp_, x => x)
+            _ <- block.add_terrain(id, location)
+            _ <- block.size.update(_ + 1)
+            _ <- size.update(_ + 1)
+          } yield ()
+
+        case t: TerrainId => ZIO.unit
+      }
     } yield ()).orElseFail(???)
 
   override def remove_terrain(id: TerrainId): IO[TerrainError, Unit] = ???
@@ -82,11 +118,24 @@ case class TerrainBlock(
     distance: Double
   ): IO[TerrainError, Unit] = ???
 }
-object TerrainBlock {
-  type QuadrantMap[+A, +B] = Map[Int, Map[Int, A] | B]
-  type M =
-    QuadrantMap[Map[Int, QuadrantMap[Any, Any]], (TerrainId, Vector[Double])]
+object TerrainBlock extends ZIOAppDefault {
 
+  private type TerrainRef = Map[Vector[Double], Ref[Terrain]] | TerrainId
+
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = for {
+    cr <- Ref.make(Vector(0.0, 0, 0))
+    tr <- Ref.make[TerrainRef](Map())
+    sz <- Ref.make(0)
+    tb = TerrainBlock(cr, tr, sz, 10)
+    _ <- tb.add_terrain("1", Vector(5, 5, 5))
+    _ <- tb.add_terrain("2", Vector(5, -5, 5))
+    _ <- ZIO.log(tb.toString)
+    sz <- tb.size.get
+    _ <- ZIO.log(sz.toString)
+  } yield ()
+
+  // generates a powerset of vectors that model
+  // all combinations of (1,-1) in each parameter
   def generate_lattice(
     src: Vector[Double],
     ind: Int = 0,
@@ -99,10 +148,4 @@ object TerrainBlock {
         ind + 1,
         Seq()
       ) ++ generate_lattice(src.updated(ind, -1), ind + 1, Seq())
-
-  def main(args: Array[String]): Unit = {
-    val res = generate_lattice(Vector(0, 0, 0, 0, 0))
-    println(res)
-    println(res.toSet.size)
-  }
 }
