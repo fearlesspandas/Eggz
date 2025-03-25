ea3bb0602b26cbb36ccffebc5b576b4524aa53a4
fearlesspandas <landonrenzullo@gmail.com>
2025-03-25 14:36:26 -0400

    deploy scripts

--- build_server.sh
+++ build_server.sh
@@ -9,16 +9,24 @@ export WORLDBLOCK_RADIUS=32768
 export RANDOMIZED_SPAWN_COUNT=200000
 export PROWLER_COUNT=50
 
-curl -L https://github.com/fearlesspandas/EggzClientUI/releases/download/Eggz-test-deploy/build_server.sh > build_server.sh
-curl -L https://github.com/fearlesspandas/EggzClientUI/releases/download/Eggz-test-deploy/physics_socket_binary > physics_socket
-curl -L https://github.com/fearlesspandas/EggzClientUI/releases/download/Eggz-test-deploy/Eggz.x86_64 > Eggz
-curl -L https://github.com/fearlesspandas/EggzClientUI/releases/download/Eggz-test-deploy/libClientPhysicsSocket.so > libClientPhysicsSocket.so
-curl -L https://github.com/fearlesspandas/EggzClientUI/releases/download/Eggz-test-deploy/libNativeTools.so > libNativeTools.so
-curl -L https://github.com/fearlesspandas/EggzClientUI/releases/download/Eggz-test-deploy/eggz-assembly-0.1.0-SNAPSHOT.jar > eggz-server.jar
+echo "retrieving deploy script" & curl -L https://github.com/fearlesspandas/EggzClientUI/releases/download/Eggz-test-deploy/build_server.sh > build_server.sh
+echo "retrieving physics socket" & curl -L https://github.com/fearlesspandas/EggzClientUI/releases/download/Eggz-test-deploy/physics_socket_binary > physics_socket
+echo "retrieving Eggz game server" & curl -L https://github.com/fearlesspandas/EggzClientUI/releases/download/Eggz-test-deploy/Eggz.x86_64 > Eggz
+echo "retrieving game socket library" & curl -L https://github.com/fearlesspandas/EggzClientUI/releases/download/Eggz-test-deploy/libClientPhysicsSocket.so > libClientPhysicsSocket.so
+echo "retrieving game client libraries" & curl -L https://github.com/fearlesspandas/EggzClientUI/releases/download/Eggz-test-deploy/libNativeTools.so > libNativeTools.so
+echo "retrieving eggz scala" & curl -L https://github.com/fearlesspandas/EggzClientUI/releases/download/Eggz-test-deploy/eggz-assembly-0.1.0-SNAPSHOT.jar > eggz-server.jar
+
+echo "Done retrieving files"
+
 
 chmod +x physics_socket
 chmod +x eggz-server.jar
+chmod +x Eggz
 
-./physics_socket $PHYSICS_HOST &&\
-	java -jar eggz-server.jar &&\
-	./Eggz
+echo "Starting Servers"
+
+./physics_socket $PHYSICS_HOST > physics_out &&\
+	java -jar eggz-server.jar > server_out &&\
+	./Eggz > game_out
+
+echo "Server components Started"
